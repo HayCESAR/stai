9ee70eb82de193eaef84fffb850e60e45e2d993f
Hayanna <hayanna@doforno-008030.local>
2022-11-27 20:31:16 -0300

    sidebar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,4 @@
-import React, { ReactNode } from 'react'
-
-import { Box, Flex, IconButton } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 import LogoStai from './logo-stai.png';
 
 export const Logo = () => (
@@ -12,6 +10,12 @@ export const Logo = () => (
     gap={4}
   >
     <Box display="flex" alignItems="center" gap={2}>
-      </Box>
+      <Image
+        borderRadius='full'
+        boxSize='50px'
+        src={LogoStai}
+        alt='STAI'
+      />
+    </Box>
   </Flex>
 );
