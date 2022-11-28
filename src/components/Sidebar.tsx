0f7bfea853a5d04c5b806553dd28b6d7b3c75354
Hayanna <hayanna@doforno-008030.local>
2022-11-28 06:49:35 -0300

    sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,8 +23,7 @@ export default function Sidebar() {
         <Flex
             pos="sticky"
             left="5"
-            h="95vh"
-            marginTop="2.5vh"
+            h="80%"
             boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
             borderRadius={navSize == "small" ? "15px" : "30px"}
             w={navSize == "small" ? "75px" : "200px"}
