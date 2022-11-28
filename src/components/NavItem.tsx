651e599277f4c5dbff12be30196ed8873c3b6e58
Hayanna <hayanna@doforno-008030.local>
2022-11-28 08:28:02 -0300

    sidebar

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -23,7 +23,7 @@ export default function NavItem({ icon, title, active, navSize }: any) {
                     p={3}
                     borderRadius={8}
                     _hover={{ textDecor: 'none', backgroundColor: "#D8D8D8" }}
-                    
+                    w={navSize == "small" ? "70%" : "100%"}
                 >
                     <MenuButton>
                         <Flex>
